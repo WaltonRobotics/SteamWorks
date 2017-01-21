1356deae75cd04fef91b24855cf9f78ba7fe5b61
NamelessSuperCoder <Marius.juston@hotmail.fr>
2017-01-21 16:07:58 -0500

    Create Mode.java

---
+++ Testbed/src/org/usfirst/frc2974/Testbed/logging/Mode.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc2974.Testbed.logging;
+
+/**
+ * Possible modes that the robot is. This enum is used for the logging.
+ */
+enum Mode {
+    TELEOP, AUTONOMOUS, TEST, DISABLED
+
+  /*
+   * There is no need to assign a modeName variable to the enum if the
+   * modeName is the exact same as the enum name. You can just use
+   * someMode.name to return the mode name however it return the EXACT name
+   * meaning capitalization is included
+    */
+}
