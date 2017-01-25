6962e18e21c604e07b6a3b9bd9efd4e7268facc6
NamelessSuperCoder <Marius.juston@hotmail.fr>
2017-01-24 22:04:14 -0500

    Formated logging code using the eclipse format function.

--- Testbed/src/org/usfirst/frc2974/Testbed/logging/Mode.java
+++ Testbed/src/org/usfirst/frc2974/Testbed/logging/Mode.java
@@ -4,12 +4,12 @@ package org.usfirst.frc2974.Testbed.logging;
  * Possible modes that the robot is. This enum is used for the logging.
  */
 enum Mode {
-    TELEOP, AUTONOMOUS, TEST, DISABLED
+	TELEOP, AUTONOMOUS, TEST, DISABLED, LOGGING
 
-  /*
-   * There is no need to assign a modeName variable to the enum if the
-   * modeName is the exact same as the enum name. You can just use
-   * someMode.name to return the mode name however it return the EXACT name
-   * meaning capitalization is included
-    */
+	/*
+	 * There is no need to assign a modeName variable to the enum if the
+	 * modeName is the exact same as the enum name. You can just use
+	 * someMode.name to return the mode name however it return the EXACT name
+	 * meaning capitalization is included
+	 */
 }
