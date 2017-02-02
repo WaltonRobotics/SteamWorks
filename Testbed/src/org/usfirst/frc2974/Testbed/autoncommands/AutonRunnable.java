97eb482c119daca9d59850a83f5689f190fb20c0
John Christian Hults <john.hults@waltonrobotics.org>
2017-02-02 17:02:15 -0500

    Testable deadreckon auton/fix
    any errors from merge fixed, also testable deadreckon auton added.

--- Testbed/src/org/usfirst/frc2974/Testbed/autoncommands/AutonRunnable.java
+++ Testbed/src/org/usfirst/frc2974/Testbed/autoncommands/AutonRunnable.java
@@ -6,39 +6,45 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class AutonRunnable extends CommandGroup {
+	
+	public TurnInTime.Direction turnDirection;
+	public TurnInTime.Direction antiTurnDirection;
+	
+	public double timeToGoal;
+	public double turnTimeToGoal;
+	
+    public enum Position{
+    	LEFT, RIGHT, CENTER
+    }
     
-    public  AutonRunnable() {
+    public  AutonRunnable(Position position, boolean doesShoot) {
     	
-    	//CENTER
-    	addSequential(null);//TODO move forward 110.3"-chassie width
-    	addSequential(null);//move backward 50"
-    	addSequential(null);//turn to goal
-    	addSequential(null);//move to goal 
-    	
-    	//LEFT/RIGHT
-    	addSequential(null);//move forward enough to turn
-    	addSequential(null);//turn opposite angle of peg
-    	addSequential(null);//move halfway to peg
-    	addSequential(null);//turn to peg
-    	addSequential(null);//move to peg
-    	addSequential(null);//reverse off peg
-    	addSequential(null);//turn to goal
-    	addSequential(null);//move to goal
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    	if(position == Position.CENTER){
+    		addSequential(new DriveStraightTrapezoid(0.4, 0.5));
+    		addSequential(new DriveStraightTrapezoid(-0.4, 0.2));
+        	addSequential(new TurnInTime(0.4,0.25,TurnInTime.Direction.CLOCKWISE));
+        	addSequential(new DriveStraightTrapezoid(0.4, 1));
+    	}else{
+    		if (position == Position.LEFT){
+    			turnDirection = TurnInTime.Direction.ANTICLOCKWISE;
+    			antiTurnDirection = TurnInTime.Direction.CLOCKWISE;
+    			timeToGoal=2;
+    			turnTimeToGoal=.25;
+    		}else{
+    			turnDirection = TurnInTime.Direction.CLOCKWISE;
+    			antiTurnDirection = TurnInTime.Direction.ANTICLOCKWISE;
+    			timeToGoal=.8;
+    			turnTimeToGoal=.8;
+    		}
+    		addSequential(new DriveStraightTrapezoid(1, 0.1));//move forward enough to turn
+        	addSequential(new TurnInTime(0.4,0.16,turnDirection));//turn opposite angle of peg
+        	addSequential(new DriveStraightTrapezoid(0.4, 0.5));//move halfway to peg
+        	addSequential(new TurnInTime(0.4,0.16,antiTurnDirection));//turn to peg
+        	addSequential(new DriveStraightTrapezoid(0.4, 0.5));//move to peg
+        	addSequential(new DriveStraightTrapezoid(-0.4, 0.5));//reverse off peg
+        	addSequential(new TurnInTime(0.4,turnTimeToGoal,turnDirection));//turn to goal
+        	addSequential(new DriveStraightTrapezoid(0.4, timeToGoal));//move to goal
+        	//addSequential(new Shoot()); TODO add shooting
+    	}
     }
 }
---
+++ Testbed/src/org/usfirst/frc2974/Testbed/autoncommands/DriveDiffTrapezoid.java
@@ -0,0 +1,129 @@
+package org.usfirst.frc2974.Testbed.autoncommands;
+
+import org.usfirst.frc2974.Testbed.Robot;
+import org.usfirst.frc2974.Testbed.logging.Mode;
+import org.usfirst.frc2974.Testbed.logging.RobotLoggerManager;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveDiffTrapezoid extends Command {
+	public static final double vmax = 1; // Value is 1 because 1 is the max
+											// velocity
+	public double amax; // Max acceleration (one of the inputs)
+	public double duration; // How long to run code (one of inputs)
+	public double t1;
+	public double t0;
+	public double dtaccel;
+	public double triTime;
+
+	private enum State {
+		ACC {
+			@Override // For accelerating portion of movement - moves to next
+						// state when done
+			public void run(DriveDiffTrapezoid d) {
+				if (d.duration / 2 < Timer.getFPGATimestamp() - d.t0) {
+					d.state = State.DEC;
+					RobotLoggerManager.setFileHandlerInstance(Mode.AUTONOMOUS, "robot.autoncommands")
+					.info("Changing state to Deceleration because half time was reached");
+					return;
+				} else if (Timer.getFPGATimestamp() >= d.t1) {
+					d.state = State.CONST;
+					RobotLoggerManager.setFileHandlerInstance(Mode.AUTONOMOUS, "robot.autoncommands")
+					.info("Changing sate to Constant because max speed reached");
+					return;
+				}
+
+				double power = (Timer.getFPGATimestamp() - d.t0) / d.dtaccel;
+
+				Robot.drivetrain.setSpeeds(power, power);
+			}
+		},
+		CONST {
+			@Override // Constant velocity portion of motion
+			public void run(DriveDiffTrapezoid d) {
+				if (d.duration - Timer.getFPGATimestamp() <= d.dtaccel) {
+					d.state = State.DEC;
+					RobotLoggerManager.setFileHandlerInstance(Mode.AUTONOMOUS, "robot.autoncommands")
+					.info("Changing state to Deceleration beacuse there is a need to start decerating to reach 0 before end");
+					
+					return;
+				}
+
+				Robot.drivetrain.setSpeeds(vmax, vmax);
+			}
+		},
+		DEC {
+			@Override // Decelerating portion of motion
+			public void run(DriveDiffTrapezoid d) {
+				if(d.duration < Timer.getFPGATimestamp() - d.t0)
+				{
+					d.state = END;
+					RobotLoggerManager.setFileHandlerInstance(Mode.AUTONOMOUS, "robot.autoncommands")
+					.info("Changing state to End beacuse the robot has reached time limit");
+					
+					return;
+				}
+				
+				double power = (d.duration - Timer.getFPGATimestamp()) / d.dtaccel;
+				Robot.drivetrain.setSpeeds(power, power);
+			}
+		},
+		END {
+			@Override // Sets speed to 0 and ends program
+			public void run(DriveDiffTrapezoid d) {
+				RobotLoggerManager.setFileHandlerInstance(Mode.AUTONOMOUS, "robot.autoncommands")
+						.info("Stopping the robot. Setting speeds to 0");
+				Robot.drivetrain.setSpeeds(0, 0);
+				d.end();
+			}
+		};
+		public void run(DriveDiffTrapezoid d) {
+		}
+	}
+
+	private State state;
+
+	public DriveDiffTrapezoid(double amax, double time) {
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.drivetrain);
+
+		this.amax = amax;
+		duration = time;
+		t0 = Timer.getFPGATimestamp();
+		t1 = vmax / amax;
+		dtaccel = t1 - t0;
+		triTime = (duration / 2) - t0;
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		state = State.ACC;
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		state.run(this);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return state == State.END; // || duration < Timer.getFPGATimestamp() - t0;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		RobotLoggerManager.setFileHandlerInstance(Mode.AUTONOMOUS, "robot.autoncommands")
+				.info("Drive straight for a duration of " + duration + " finished. Setting speeds to 0");
+		Robot.drivetrain.setSpeeds(0, 0);
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
+}
