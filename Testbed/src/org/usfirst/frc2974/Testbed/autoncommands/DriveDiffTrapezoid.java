ac40972aa3cd6d8c2b8dabfdccdd46a92f01fa80
Soham Sonthi <sohamsonthi@gmail.com>
2017-02-04 09:28:27 -0500

    Time instances for stopping

--- Testbed/src/org/usfirst/frc2974/Testbed/autoncommands/DriveDiffTrapezoid.java
+++ Testbed/src/org/usfirst/frc2974/Testbed/autoncommands/DriveDiffTrapezoid.java
@@ -142,7 +142,7 @@ public class DriveDiffTrapezoid extends Command {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return state == State.END; // || duration < Timer.getFPGATimestamp() - t0;
+		return state == State.END || duration < Timer.getFPGATimestamp() - t0;
 	}
 
 	// Called once after isFinished returns true
--- Testbed/src/org/usfirst/frc2974/Testbed/autoncommands/DriveStraightTrapezoid.java
+++ Testbed/src/org/usfirst/frc2974/Testbed/autoncommands/DriveStraightTrapezoid.java
@@ -126,7 +126,7 @@ public class DriveStraightTrapezoid extends Command {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return state == State.END; // || duration < Timer.getFPGATimestamp() - t0;
+		return state == State.END || duration < Timer.getFPGATimestamp() - t0;
 	}
 
 	// Called once after isFinished returns true
