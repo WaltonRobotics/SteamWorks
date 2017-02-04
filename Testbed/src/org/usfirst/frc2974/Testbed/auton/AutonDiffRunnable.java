66b7c680c171f83b29e434a4dc6ab34d14a5410d
Allison <alcp2610@gmail.com>
2017-02-04 11:22:48 -0500

    Fixed auton selector
    fixed time handling so selector works

--- Testbed/src/org/usfirst/frc2974/Testbed/auton/AutonDiffRunnable.java
+++ Testbed/src/org/usfirst/frc2974/Testbed/auton/AutonDiffRunnable.java
@@ -16,6 +16,8 @@ public class AutonDiffRunnable extends CommandGroup {
 	
 	public DriveDiffTrapezoid.DiffDirection diffDirection;
 	public DriveDiffTrapezoid.DiffDirection antiDiffDirection;
+	public Position position;
+	public boolean doesShoot;
 	
 	public Command toGoalCommand;
 	
@@ -24,28 +26,38 @@ public class AutonDiffRunnable extends CommandGroup {
     }
     
     public  AutonDiffRunnable(Position position, boolean doesShoot) {
+    	this.position = position;
+    	this.doesShoot = doesShoot;
+    	switch(position) {
     	
-    	if(position == Position.CENTER){
+    	case CENTER:
     		addSequential(new DriveStraightTrapezoid(0.7, 1.1,DriveStraightTrapezoid.Direction.FORWARD));
     		addSequential(new DriveStraightTrapezoid(0.5, 1.8,DriveStraightTrapezoid.Direction.ANTIFORWARD));
         	addSequential(new TurnInTime(0.4,0.25,TurnInTime.Direction.CLOCKWISE));//check
         	addSequential(new DriveStraightTrapezoid(0.7, 2,DriveStraightTrapezoid.Direction.FORWARD));//check
-    	}else{
-    		if (position == Position.LEFT) {
-    			diffDirection = DriveDiffTrapezoid.DiffDirection.ANTICLOCKWISE;
-    			antiDiffDirection = DriveDiffTrapezoid.DiffDirection.ANTICLOCKWISEBACK;
-    			toGoalCommand = new DriveStraightTrapezoid(0.7, 4.2,DriveStraightTrapezoid.Direction.FORWARD);
-    		}else{
-    			diffDirection = DriveDiffTrapezoid.DiffDirection.CLOCKWISE;
-    			antiDiffDirection = DriveDiffTrapezoid.DiffDirection.CLOCKWISEBACK;
-    			toGoalCommand = new DriveDiffTrapezoid(0.7, 4.8, 0.65,DriveDiffTrapezoid.DiffDirection.ANTICLOCKWISE);
-    		}
-    		addSequential(new DriveDiffTrapezoid(0.7, 1.8, 0.65,diffDirection));//move onto peg
-        	addSequential(new DriveDiffTrapezoid(0.7, 3,0.2,antiDiffDirection));//reverse off peg
-        	addSequential(toGoalCommand);//move to goal
-        	if(doesShoot){
-            	//addSequential(new Shoot()); TODO add shooting
-        	}
+    		break;
+    		
+    	case LEFT:
+    		directionAuton(DriveDiffTrapezoid.DiffDirection.ANTICLOCKWISE,
+    				DriveDiffTrapezoid.DiffDirection.ANTICLOCKWISEBACK,
+    				new DriveStraightTrapezoid(0.7,4.2,DriveStraightTrapezoid.Direction.FORWARD));
+    		break;
+    		
+    	case RIGHT:
+    		directionAuton(DriveDiffTrapezoid.DiffDirection.CLOCKWISE,
+    				DriveDiffTrapezoid.DiffDirection.CLOCKWISEBACK,
+    				new DriveDiffTrapezoid(0.7,4.8,0.65,DriveDiffTrapezoid.DiffDirection.ANTICLOCKWISE));
+    		break;
     	}
+        if(doesShoot){
+            //addSequential(new Shoot()); TODO add shooting
+        }
     }
+
+	private void directionAuton(DiffDirection diffDirection, DiffDirection antiDiffDirection,
+			Command toGoalCommand) {
+		addSequential(new DriveDiffTrapezoid(0.7, 1.8, 0.65,diffDirection));//move onto peg
+    	addSequential(new DriveDiffTrapezoid(0.7, 3,0.2,antiDiffDirection));//reverse off peg
+    	addSequential(toGoalCommand);
+	}
 }
