0033bd0bbd3775a990d57ba41af48c86010fbe2a
Tim Stanistreet <tim.stanistreet@waltonrobotics.org>
2017-02-04 12:50:13 -0500

    Removed extraneous files

---
+++ Test1/src/com/stanistreet/RobotParameters.java
@@ -0,0 +1,16 @@
+package com.stanistreet;
+
+public final class RobotParameters {
+
+	static final float width = 0.12F;
+	static final float vMax = 0.25F;
+	static final float aMax = 1.0F;
+	
+	static final float wheelRadius = 0.035F;
+	
+	static final float kFeedForwardVelocity = 100F / 0.5F;
+	static final float kFeedForwardAccel = 10;
+	static final float kFeedBackProportional = 1000;
+	static final float kFeedBackIntegral = 0;
+	static final float kFeedBackDifferential = 0;
+}
