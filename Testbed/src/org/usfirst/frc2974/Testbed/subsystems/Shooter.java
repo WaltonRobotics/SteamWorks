cce49fedf7b0b8910264cbc3ef606e84219fc00e
ThundrHawk <rnewtonkids001@gmail.com>
2017-02-04 14:23:01 -0500

    Shoot Stuff

--- Testbed/src/org/usfirst/frc2974/Testbed/subsystems/Shooter.java
+++ Testbed/src/org/usfirst/frc2974/Testbed/subsystems/Shooter.java
@@ -1,21 +1,44 @@
 package org.usfirst.frc2974.Testbed.subsystems;
 
 import org.usfirst.frc2974.Testbed.Robot;
-import org.usfirst.frc2974.Testbed.autoncommands.Aim;
+import org.usfirst.frc2974.Testbed.RobotMap;
 
+import com.ctre.talonsrx.CANTalon;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- */
-public class Shooter extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+public class Shooter extends Subsystem{
+	
+	private CANTalon flywheelMotor;
+	private DigitalOutput indexer;
+	
+	private final double SPEED = 6000; //rpm
+	
+	public Shooter() {
+		flywheelMotor = RobotMap.flywheelMotor;
+	}
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        setDefaultCommand(Robot.aim);
-    }
+	@Override
+	protected void initDefaultCommand() {
+		setDefaultCommand(Robot.aim);
+	}
+	
+	public void enable() {
+		flywheelMotor.set(SPEED);
+	}
+	
+	public void disable() {
+		flywheelMotor.set(0);
+	}	
+	
+	public boolean isAtSpeed() {
+		return (60*102.4*flywheelMotor.getSpeed()) >= SPEED;
+		//60 sec/min * 10 ticks/sec * 1 rev/1024 ticks = rpm
+	}
+	
+	public void index() {
+		indexer.set(true);
+	}
+
 }
-
