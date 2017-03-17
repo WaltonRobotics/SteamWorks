723f047c34fc9e60d6669533de1faab2f3efcfa7
NamelessSuperCoder <Marius.juston@hotmail.fr>
2017-03-16 21:22:00 -0400

    Update picamera_peg.py
    changed what status peg returns, removed useless smartdashboard information

--- OpenCV Test Project/src/picamera_peg.py
+++ OpenCV Test Project/src/picamera_peg.py
@@ -15,13 +15,13 @@ import os
 
 PID_FILE_NAME = "pid.pid"
 
-try:
-    os.remove(PID_FILE_NAME)
-except IOError:
-    pass
+#try:
+#    os.remove(PID_FILE_NAME)
+#except IOError:
+#    pass
 
-with open(PID_FILE_NAME, "w") as fd:    
-    fd.write("{0:d}\n".format(os.getpid()))
+#with open(PID_FILE_NAME, "w") as fd:    
+#    fd.write("{0:d}\n".format(os.getpid()))
 
 
 image = np.empty((240, 320, 3), dtype=np.uint8)
@@ -42,32 +42,35 @@ def camera_setup(camera):
     camera.resolution = (320, 240)
     camera.framerate = 60
     camera.iso = 100
-    camera.shutter_speed = 1000
+    camera.shutter_speed = 2000
     camera.awb_mode = "off"
     camera.awb_gains = (Fraction(135, 128), Fraction(343, 128))
     time.sleep(2)
     camera.exposure_mode = "off"
 
-def find_angle(distance1, distance2):
-    angle_valid = False
+#def find_angle(distance1, distance2):
+#    angle_valid = False
     
-    delta_d = distance2 - distance1    
+#    delta_d = distance2 - distance1    
     #print(delta_d)
 
-    angle = ""
+#    angle = ""
     
-    if math.fabs(delta_d) < KNOWN_HYPOTENEUSE:
-        angle = math.asin(delta_d / KNOWN_HYPOTENEUSE) if math.asin(delta_d / KNOWN_HYPOTENEUSE) else ""
-        if not isinstance(angle, str):
-            angle_vaild = True
+#    if math.fabs(delta_d) < KNOWN_HYPOTENEUSE:
+#        angle = math.asin(delta_d / KNOWN_HYPOTENEUSE) if math.asin(delta_d / KNOWN_HYPOTENEUSE) else ""
+#        if not isinstance(angle, str):
+#            angle_vaild = True
 
-    sd.putBoolean("Valid angle peg", angle_valid)
+#    sd.putBoolean("Valid angle peg", angle_valid)
 
 
-    return angle, angle_valid
+#    return angle, angle_valid
 
 with picamera.PiCamera() as camera:
     camera_setup(camera)
+
+    sd.putNumber("resolutionX", camera.resolution[0])
+    sd.putNumber("resolutionY", camera.resolution[1])
     
     camera.start_preview(fullscreen=False, window=(640,640,320,240))
     #print(type(sd))
@@ -81,11 +84,6 @@ with picamera.PiCamera() as camera:
         #print(camera.digital_gain, camera.analog_gain)
         #cv2.imshow('img', image)
         if len(gripped.filter_contours_output) >= 2:
-            if len(gripped.filter_contours_output) == 2:
-                sd.putString("status peg", "correct number of contours")
-
-            else:
-                sd.putString("status peg", "too many contours")
             #print(gripped.filter_contours_output)
             #print(gripped.filter_contours_output[0].shape)
             #print(type(cv2.minAreaRect(gripped.filter_contours_output[0])))
@@ -93,11 +91,11 @@ with picamera.PiCamera() as camera:
             #print(distance)
 
             cY = 0
-            distance = 0
+            #distance = 0
             cX = 0
             
-            distance1 = 0
-            distance2 = 0
+            #distance1 = 0
+            #distance2 = 0
             
             lcv = 0
 
@@ -113,17 +111,17 @@ with picamera.PiCamera() as camera:
                          Y1 = int(M["m01"] / M["m00"])
                          width1 = boundries[1][0]
 
-                         distance1 = KNOWN_WIDTH * KNOWN_FOCAL_LENGTH / boundries[1][0]
+                         #distance1 = KNOWN_WIDTH * KNOWN_FOCAL_LENGTH / boundries[1][0]
                     elif (lcv == 2):
                          X2 = int(M["m10"] / M["m00"])
                          Y2 = int(M["m01"] / M["m00"])
                          width2 = boundries[1][0]
-                         distance2 = KNOWN_WIDTH * KNOWN_FOCAL_LENGTH / boundries[1][0]
+                         #distance2 = KNOWN_WIDTH * KNOWN_FOCAL_LENGTH / boundries[1][0]
 
                     #print(boundries[1][0])
 
                     cX += int(M["m10"] / M["m00"])
-                    distance += KNOWN_WIDTH * KNOWN_FOCAL_LENGTH / boundries[1][0]
+                    #distance += KNOWN_WIDTH * KNOWN_FOCAL_LENGTH / boundries[1][0]
                     cY += int(M["m01"] / M["m00"])
                     #cv2.drawContours(image, [c], -1, (255,0,0),2)
                   
@@ -132,25 +130,25 @@ with picamera.PiCamera() as camera:
 
             #print(distance1, distance2)
 
-            (tilt_angle, is_valid_angle) = find_angle(distance1, distance2) 
+            #(tilt_angle, is_valid_angle) = find_angle(distance1, distance2) 
 
-            print(tilt_angle)
+            #print(tilt_angle)
 
             cY /= length
             cX /= length
-            distance /= length
+            #distance /= length
 
             if (abs(X1-X2) > abs(Y1-Y2)):
                 sd.putNumber("Center point X peg", cX)
                 sd.putNumber("Center point Y peg", cY)
-                sd.putNumber("Camera distance peg", distance)
+                #sd.putNumber("Camera distance peg", distance)
 
                 if is_valid_angle:
                     sd.putNumber("Target angle peg", tilt_angle)
             #sd.putNumberArray("image", gripped.filter_contours_output)
             #break
-        else:
-            sd.putString("status peg", "too few contours")
+        
+        sd.putNumber("status peg", len(gripped.filter_contours_output))
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
