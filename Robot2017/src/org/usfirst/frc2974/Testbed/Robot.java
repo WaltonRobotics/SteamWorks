7e8192fccb20c4aaf21067a6d9cbdf36a68d8986
ThundrHawk <rnewtonkids001@gmail.com>
2017-03-22 19:13:35 -0400

    Push before revert

--- Robot2017/src/org/usfirst/frc2974/Testbed/Robot.java
+++ Robot2017/src/org/usfirst/frc2974/Testbed/Robot.java
@@ -33,207 +33,217 @@ import org.usfirst.frc2974.Testbed.subsystems.*;
  */
 public class Robot extends IterativeRobot {
 
-    Command autonomousCommand;
+	Command autonomousCommand;
 
-    public static OI oi;
-    private SendableChooser<Command> autoChooser;
-    public static Drivetrain drivetrain;
-    public static PoseEstimator poseEstimator;
-    public static Shooter shooter;
-    public static Hopper hopper;
-    public static Intake intake;
-    public static Climber climber;
-    public static Aim aim;
+	public static OI oi;
+	private SendableChooser<Command> autoChooser;
+	public static Drivetrain drivetrain;
+	public static PoseEstimator poseEstimator;
+	public static Shooter shooter;
+	public static Hopper hopper;
+	public static Intake intake;
+	public static Climber climber;
+	public static Aim aim;
 
 	public static GearIntake gearIntake;
-    
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-	    RobotMap.init();
-	    declarePrefs();
 
-    	poseEstimator = new PoseEstimator();
-        drivetrain = new Drivetrain();
-        shooter = new Shooter();
-        hopper = new Hopper();
-        intake = new Intake();
-        climber = new Climber();
-        aim = new Aim();
-        gearIntake = new GearIntake();
-        
-        // OI must be constructed after subsystems. If the OI creates Commands
-        //(which it very likely will), subsystems are not guaranteed to be
-        // constructed yet. Thus, their requires() statements may grab null
-        // pointers. Bad news. Don't move it.
-        oi = new OI();
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
+		RobotMap.init();
+		declarePrefs();
 
-        createAutonomousChooser();
-        SmartDashboard.putNumber("Duration", 0);
-        SmartDashboard.putNumber("aMax", 0);    
-        SmartDashboard.putNumber("DiffPercent", 0);  
-        SmartDashboard.putNumber("Distance", 0);  
-        SmartDashboard.putNumber("vCruise", 0);  
-        SmartDashboard.putNumber("encoderDistance", 0);
-        SmartDashboard.putNumber("encoderAccel", 0);
-        SmartDashboard.putNumber("encoderSpeed", 0);
-        SmartDashboard.putNumber("encoderAngle", 0);
-        SmartDashboard.putBoolean("isForwards", true);
-        SmartDashboard.putBoolean("captureImage", false);
-    }
-    
-    private void declarePrefs() {
-    	boolean reset;
-	    Preferences pref = Preferences.getInstance();
-	    reset = pref.getBoolean("reset", false);
-	    
-	    if(reset || !pref.containsKey("isCompBot")) {
-	    	pref.putBoolean("isCompBot", true);
-	    }
-	    
-	    Drivetrain.declarePrefs(reset);
-	    Shooter.declarePrefs(reset);
-	    Climber.declarePrefs(reset);
-	    
-	    pref.putBoolean("reset", false);
-    }
-	    
-    /**
-     * This function is called when the disabled button is hit.
-     * You can use it to reset subsystems before shutting down.
-     */
-    public void disabledInit(){
-    	SmartDashboard.putBoolean("captureImage", false);
-    	
-//    	CameraServer.getInstance().removeCamera("cam0");
-    	
-    }
+		poseEstimator = new PoseEstimator();
+		drivetrain = new Drivetrain();
+		shooter = new Shooter();
+		hopper = new Hopper();
+		intake = new Intake();
+		climber = new Climber();
+		aim = new Aim();
+		gearIntake = new GearIntake();
 
-    public void disabledPeriodic() {
-        Scheduler.getInstance().run();
-        dumpSmartDashboardValues();
-    }
+		// OI must be constructed after subsystems. If the OI creates Commands
+		// (which it very likely will), subsystems are not guaranteed to be
+		// constructed yet. Thus, their requires() statements may grab null
+		// pointers. Bad news. Don't move it.
+		oi = new OI();
 
-    public void autonomousInit() {
-    	SmartDashboard.putBoolean("captureImage", true);
-    	
-    	drivetrain.shiftDown();
-    	try {
-    		autonomousCommand = autoChooser.getSelected();
-    		autonomousCommand.start();
-    	}
-    	catch (NullPointerException e){    	
-    		autonomousCommand =  new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED2);
-    		autonomousCommand.start();
-    	}
-    }
+		createAutonomousChooser();
+		SmartDashboard.putNumber("Duration", 0);
+		SmartDashboard.putNumber("aMax", 0);
+		SmartDashboard.putNumber("DiffPercent", 0);
+		SmartDashboard.putNumber("Distance", 0);
+		SmartDashboard.putNumber("vCruise", 0);
+		SmartDashboard.putNumber("encoderDistance", 0);
+		SmartDashboard.putNumber("encoderAccel", 0);
+		SmartDashboard.putNumber("encoderSpeed", 0);
+		SmartDashboard.putNumber("encoderAngle", 0);
+		SmartDashboard.putBoolean("isForwards", true);
+		SmartDashboard.putBoolean("captureImage", false);
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-    	//poseEstimator.updatePose();
-    	dumpSmartDashboardValues();
-        Scheduler.getInstance().run();
-        
-    }
-    
-    private void createAutonomousChooser(){
-    	
-    	autoChooser = new SendableChooser<Command>();
-    	autoChooser.addDefault("Do Nothing", null);
-    	autoChooser.addObject("CrossLine", new DriveStraightTrapezoid(false,1,0.6,DriveStraightTrapezoid.Direction.ANTIFORWARD));
-    	autoChooser.addObject("Red 1", new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED1));
-    	autoChooser.addObject("Red 2", new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED2));
-    	autoChooser.addObject("Red 3", new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED3));
-    	autoChooser.addObject("Blue 1", new AutonEncoderToPeg(AutonEncoderToPeg.Position.BLUE1));
-    	autoChooser.addObject("Blue 2", new AutonEncoderToPeg(AutonEncoderToPeg.Position.BLUE2));
-    	autoChooser.addObject("Blue 3", new AutonEncoderToPeg(AutonEncoderToPeg.Position.BLUE3));
-    	
-    	//autoChooser.addObject("Auton move to peg and boiler", new AutonEncoderWithVision(true));
-    	//autoChooser.addObject("Auton move forward past line", new AutonEncoderWithVision(false));
-    	SmartDashboard.putData("Auto", autoChooser);
-    }
+	private void declarePrefs() {
+		boolean reset;
+		Preferences pref = Preferences.getInstance();
+		reset = pref.getBoolean("reset", false);
 
-    public void teleopInit() {
-    	 SmartDashboard.putBoolean("captureImage", false);
-    	
-        // This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
-      //  RobotMap.compressor.start();
-        createTestButtons();
+		if (reset || !pref.containsKey("isCompBot")) {
+			pref.putBoolean("isCompBot", true);
+		}
 
-        Scheduler.getInstance().add(aim);	//TODO add other commands
-        
-//        CameraServer.getInstance().addServer("server");
-//        CameraServer.getInstance().startAutomaticCapture("cam0", 0);
-//        CameraServer.getInstance().startAutomaticCapture();
-    }
+		Drivetrain.declarePrefs(reset);
+		Shooter.declarePrefs(reset);
+		Climber.declarePrefs(reset);
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-    	//poseEstimator.updatePose();
-    	dumpSmartDashboardValues();
-        Scheduler.getInstance().run();
-    }
-    
-    public void createTestButtons(){
-    	
-      	SmartDashboard.putData("MoveTimeForward",new DriveStraightTrapezoid(true, SmartDashboard.getNumber("aMax",0)
-    			,SmartDashboard.getNumber("Duration",0), DriveStraightTrapezoid.Direction.FORWARD));
-      	SmartDashboard.putData("MoveTimeAntiforward",new DriveStraightTrapezoid(true, SmartDashboard.getNumber("aMax",0)
-    			,SmartDashboard.getNumber("Duration",0), DriveStraightTrapezoid.Direction.ANTIFORWARD));
-      	SmartDashboard.putData("DiffTimeClockWise",new DriveDiffTrapezoid(true, SmartDashboard.getNumber("aMax",0)
-    			,SmartDashboard.getNumber("Duration",0)
-    			,SmartDashboard.getNumber("DiffPercent", 0)
-    			,DriveDiffTrapezoid.DiffDirection.CLOCKWISE));
-      	SmartDashboard.putData("DiffTimeAntiClockWise",new DriveDiffTrapezoid(true, SmartDashboard.getNumber("aMax",0)
-    			,SmartDashboard.getNumber("Duration",0)
-    			,SmartDashboard.getNumber("DiffPercent", 0)
-    			,DriveDiffTrapezoid.DiffDirection.ANTICLOCKWISE));
-      	SmartDashboard.putData("DiffTimeClockWiseBack",new DriveDiffTrapezoid(true, SmartDashboard.getNumber("aMax",0)
-    			,SmartDashboard.getNumber("Duration",0)
-    			,SmartDashboard.getNumber("DiffPercent", 0)
-    			,DriveDiffTrapezoid.DiffDirection.CLOCKWISEBACK));
-      	SmartDashboard.putData("DiffTimeAntiClockWiseBack",new DriveDiffTrapezoid(true, SmartDashboard.getNumber("aMax",0)
-    			,SmartDashboard.getNumber("Duration",0)
-    			,SmartDashboard.getNumber("DiffPercent", 0)
-    			,DriveDiffTrapezoid.DiffDirection.ANTICLOCKWISEBACK));
+		pref.putBoolean("reset", false);
+	}
+
+	/**
+	 * This function is called when the disabled button is hit. You can use it
+	 * to reset subsystems before shutting down.
+	 */
+	public void disabledInit() {
+		SmartDashboard.putBoolean("captureImage", false);
+
+		// CameraServer.getInstance().removeCamera("cam0");
+
+	}
+
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+		dumpSmartDashboardValues();
+	}
+
+	public void autonomousInit() {
+		SmartDashboard.putBoolean("captureImage", true);
+
+		drivetrain.shiftDown();
+		try {
+			autonomousCommand = autoChooser.getSelected();
+			autonomousCommand.start();
+		} catch (NullPointerException e) {
+			autonomousCommand = new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED2);
+			autonomousCommand.start();
+		}
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		// poseEstimator.updatePose();
+		dumpSmartDashboardValues();
+		Scheduler.getInstance().run();
+
+	}
+
+	private void createAutonomousChooser() {
+
+		autoChooser = new SendableChooser<Command>();
+		autoChooser.addDefault("Do Nothing", null);
+		autoChooser.addObject("CrossLine",
+				new DriveStraightTrapezoid(false, 1, 0.6, DriveStraightTrapezoid.Direction.ANTIFORWARD));
+		autoChooser.addObject("Red 1", new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED1));
+		autoChooser.addObject("Red 2", new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED2));
+		autoChooser.addObject("Red 3", new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED3));
+		autoChooser.addObject("Blue 1", new AutonEncoderToPeg(AutonEncoderToPeg.Position.BLUE1));
+		autoChooser.addObject("Blue 2", new AutonEncoderToPeg(AutonEncoderToPeg.Position.BLUE2));
+		autoChooser.addObject("Blue 3", new AutonEncoderToPeg(AutonEncoderToPeg.Position.BLUE3));
+		autoChooser.addObject("Red 1 and Move Forward", new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED1GF));
+		autoChooser.addObject("Red 3 and Move Forward", new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED3GF));
+		autoChooser.addObject("Blue 1 and Move Forward", new AutonEncoderToPeg(AutonEncoderToPeg.Position.BLUE1GF));
+		autoChooser.addObject("Blue 3 and Move Forward", new AutonEncoderToPeg(AutonEncoderToPeg.Position.BLUE3GF));
+
+		// autoChooser.addObject("Auton move to peg and boiler", new
+		// AutonEncoderWithVision(true));
+		// autoChooser.addObject("Auton move forward past line", new
+		// AutonEncoderWithVision(false));
+		SmartDashboard.putData("Auto", autoChooser);
+	}
+
+	public void teleopInit() {
+		SmartDashboard.putBoolean("captureImage", false);
+
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomousCommand != null)
+			autonomousCommand.cancel();
+		// RobotMap.compressor.start();
+		createTestButtons();
+
+		Scheduler.getInstance().add(aim); // TODO add other commands
+
+		// CameraServer.getInstance().addServer("server");
+		// CameraServer.getInstance().startAutomaticCapture("cam0", 0);
+		// CameraServer.getInstance().startAutomaticCapture();
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		// poseEstimator.updatePose();
+		dumpSmartDashboardValues();
+		Scheduler.getInstance().run();
+	}
+
+	public void createTestButtons() {
+
+		SmartDashboard.putData("MoveTimeForward", new DriveStraightTrapezoid(true, SmartDashboard.getNumber("aMax", 0),
+				SmartDashboard.getNumber("Duration", 0), DriveStraightTrapezoid.Direction.FORWARD));
+		SmartDashboard.putData("MoveTimeAntiforward",
+				new DriveStraightTrapezoid(true, SmartDashboard.getNumber("aMax", 0),
+						SmartDashboard.getNumber("Duration", 0), DriveStraightTrapezoid.Direction.ANTIFORWARD));
+		SmartDashboard.putData("DiffTimeClockWise",
+				new DriveDiffTrapezoid(true, SmartDashboard.getNumber("aMax", 0),
+						SmartDashboard.getNumber("Duration", 0), SmartDashboard.getNumber("DiffPercent", 0),
+						DriveDiffTrapezoid.DiffDirection.CLOCKWISE));
+		SmartDashboard.putData("DiffTimeAntiClockWise",
+				new DriveDiffTrapezoid(true, SmartDashboard.getNumber("aMax", 0),
+						SmartDashboard.getNumber("Duration", 0), SmartDashboard.getNumber("DiffPercent", 0),
+						DriveDiffTrapezoid.DiffDirection.ANTICLOCKWISE));
+		SmartDashboard.putData("DiffTimeClockWiseBack",
+				new DriveDiffTrapezoid(true, SmartDashboard.getNumber("aMax", 0),
+						SmartDashboard.getNumber("Duration", 0), SmartDashboard.getNumber("DiffPercent", 0),
+						DriveDiffTrapezoid.DiffDirection.CLOCKWISEBACK));
+		SmartDashboard.putData("DiffTimeAntiClockWiseBack",
+				new DriveDiffTrapezoid(true, SmartDashboard.getNumber("aMax", 0),
+						SmartDashboard.getNumber("Duration", 0), SmartDashboard.getNumber("DiffPercent", 0),
+						DriveDiffTrapezoid.DiffDirection.ANTICLOCKWISEBACK));
 		SmartDashboard.putData("SetMotionControllerConstants", new SetMotionControllerConstants());
 
 		SmartDashboard.putData("MotionEncoder", new DriveStraightByEncoder(true, 0, 0, 0));
 		SmartDashboard.putData("TurnByEncoder", new DriveTurnByEncoder(true, 0, 0, 0));
 		SmartDashboard.putData("TurnBySpline", new DriveSplineByEncoder(true, 0, 0, 0, 0));
-		
+
 		SmartDashboard.putData("testShooterPower", new TestShooterPower());
-//		SmartDashboard.putData("testToPegCenter", new AutonEncoderToPeg(AutonEncoderToPeg.Position.CENTER));
-//		SmartDashboard.putData("testToPegLeft", new AutonEncoderToPeg(AutonEncoderToPeg.Position.RED));
-//		SmartDashboard.putData("testToPegRight", new AutonEncoderToPeg(AutonEncoderToPeg.Position.RIGHT));
-    }
+		// SmartDashboard.putData("testToPegCenter", new
+		// AutonEncoderToPeg(AutonEncoderToPeg.Position.CENTER));
+		// SmartDashboard.putData("testToPegLeft", new
+		// AutonEncoderToPeg(AutonEncoderToPeg.Position.RED));
+		// SmartDashboard.putData("testToPegRight", new
+		// AutonEncoderToPeg(AutonEncoderToPeg.Position.RIGHT));
+	}
 
-    @Override
-    public void testInit() {
-    }
+	@Override
+	public void testInit() {
+	}
 
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
-    
-    private void dumpSmartDashboardValues () {
-        drivetrain.dumpSmartdashboardValues();
-        poseEstimator.dumpSmartdashboardValues();
-        shooter.dumpValuesToSmartDashboard();
-        climber.dumpSmartdashboardValues();
-    }
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
+
+	private void dumpSmartDashboardValues() {
+		drivetrain.dumpSmartdashboardValues();
+		poseEstimator.dumpSmartdashboardValues();
+		shooter.dumpValuesToSmartDashboard();
+		climber.dumpSmartdashboardValues();
+	}
 }
-
--- Robot2017/src/org/usfirst/frc2974/Testbed/autoncommands/AutonEncoderToPeg.java
+++ Robot2017/src/org/usfirst/frc2974/Testbed/autoncommands/AutonEncoderToPeg.java
@@ -40,7 +40,7 @@ public class AutonEncoderToPeg extends Command {
 	private static final double HOLD_POWER = 0.25;
 
 	public enum Position {
-		RED1, RED2, RED3, BLUE1, BLUE2, BLUE3
+		RED1, RED2, RED3, BLUE1, BLUE2, BLUE3, RED1GF, RED3GF, BLUE1GF, BLUE3GF
 	}
 
 	private Drivetrain driveTrain;
@@ -126,7 +126,36 @@ public class AutonEncoderToPeg extends Command {
 
 		driveTrain.addControllerMotion(toPeg);
 		driveTrain.addControllerMotion(new MotionPathStraight(toPeg.getFinalPose(), -0.3, PEG_SPEED, PEG_ACCELERATION));
+		
+	}
+	
+	private void addDriveParametersRed1GF() {
+		driveTrain.cancelMotion();
 
+		double offset = Preferences.getInstance().getDouble("drivetrain.offsetRed1", 0);
+
+		Pose peg = new Pose(PEG_POINT_RED1, -PEG_ANGLE);
+		peg.offsetPoint(-offset);
+
+		MotionProvider toPeg = new MotionPathSpline(ZERO, PEG_START_LENGTH + offset, peg, PEG_END_LENGTH, MAX_SPEED,
+				MAX_ACCELERATION, false);
+
+		driveTrain.addControllerMotion(toPeg);
+		driveTrain.addControllerMotion(new MotionPathStraight(toPeg.getFinalPose(), -0.3, PEG_SPEED, PEG_ACCELERATION));
+		
+		
+	}
+	
+	private void addDriveParametersRed3GF() {
+		
+	}
+	
+	private void addDriveParametersBlue1GF() {
+		
+	}
+	
+	private void addDriveParametersBlue3GF() {
+		
 	}
 
 	@Override
@@ -153,6 +182,18 @@ public class AutonEncoderToPeg extends Command {
 			break;
 		case BLUE1:
 			addDriveParametersBlue1();
+			break;
+		case RED1GF:
+			addDriveParametersRed1GF();
+			break;
+		case RED3GF:
+			addDriveParametersRed3GF();
+			break;
+		case BLUE1GF:
+			addDriveParametersBlue1GF();
+			break;
+		case BLUE3GF:
+			addDriveParametersBlue3GF();
 			break;
 		}
 
