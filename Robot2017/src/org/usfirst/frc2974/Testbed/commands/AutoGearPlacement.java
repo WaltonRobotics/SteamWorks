36e676e433141a05f392dd7280c5226468a4728e
NamelessSuperCoder <Marius.juston@hotmail.fr>
2017-04-15 19:25:19 -0400

    created all the states.
    needs testing. need to change how this command is started as for now if
    always runs. need to find a way to get an acurate distance using the
    camera

--- Robot2017/src/org/usfirst/frc2974/Testbed/commands/AutoGearPlacement.java
+++ Robot2017/src/org/usfirst/frc2974/Testbed/commands/AutoGearPlacement.java
@@ -1,7 +1,14 @@
 package org.usfirst.frc2974.Testbed.commands;
 
 import org.usfirst.frc2974.Testbed.Robot;
+import org.usfirst.frc2974.Testbed.autoncommands.DriveStraightByEncoder;
+import org.usfirst.frc2974.Testbed.controllers.MotionPathSpline;
+import org.usfirst.frc2974.Testbed.controllers.MotionPathStraight;
+import org.usfirst.frc2974.Testbed.controllers.MotionProvider;
+import org.usfirst.frc2974.Testbed.controllers.Point2D;
+import org.usfirst.frc2974.Testbed.controllers.Pose;
 
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -16,7 +23,8 @@ public class AutoGearPlacement extends Command {
 		LOOKING {
 			@Override
 			State run() {
-				if (!Robot.oi.autoGearPlacement.get() && SmartDashboard.getNumber(CAMERA_CENTER_X_KEY, 0) == 0)
+				if (!Robot.oi.autoGearPlacement.get() && SmartDashboard.getNumber(CAMERA_CENTER_X_KEY, 0) == 0
+						&& !Robot.gearIntake.hasGear())
 					return super.run();
 
 				return State.MOVE_TO_PEG;
@@ -24,10 +32,43 @@ public class AutoGearPlacement extends Command {
 		},
 		MOVE_TO_PEG {
 			@Override
+			void init() {
+				Robot.drivetrain.cancelMotion();
+				
+				Robot.drivetrain.addControllerMotion(createCameraSpline(0));
+				
+				Robot.drivetrain.startMotion();
+			}
+
+			@Override
 			State run() {
 				// TODO move the robot to pegh using spline
 				// TODO figure out how to use the distance parameter
-				return super.run();
+				if(Robot.drivetrain.isControllerFinished())
+					return State.WAIT_FOR_GEAR_REMOVAL;
+					
+					return super.run();
+			}
+			
+			private final Pose ZERO = new Pose(new Point2D(0, 0), 0);
+			
+			public static final double PEG_SPEED = 1;
+			public static final double PEG_ACCELERATION = 1;
+			
+			private MotionProvider createCameraSpline(double offset) {
+				Robot.drivetrain.cancelMotion();
+
+				double x = -1.016 - offset;
+				double y = SmartDashboard.getNumber("mm peg centerX", 0) / 1000;
+				double controlPoint = Math.abs(x / 2);
+
+				Point2D pegPoint = new Point2D(x, y);
+
+				Pose peg = new Pose(pegPoint, 0);
+
+				System.out.println(y);
+
+				return new MotionPathSpline(ZERO, controlPoint, peg, controlPoint, PEG_SPEED, PEG_ACCELERATION, false);
 			}
 		},
 		WAIT_FOR_GEAR_REMOVAL {
@@ -42,29 +83,52 @@ public class AutoGearPlacement extends Command {
 		WAIT_FOR_BACKING {
 
 			double startTime;
+			double delay;
 
 			@Override
 			void init() {
 				startTime = Timer.getFPGATimestamp();
+				delay = Preferences.getInstance().getDouble("drivetrain.pegDelay", 2.5);
 			}
 
 			@Override
 			State run() {
+				if (Timer.getFPGATimestamp() - startTime < delay)
+					return super.run();
 
-				return super.run();
+				return State.BACK_UP;
 			}
 		},
 		BACK_UP {
 			@Override
+			void init() {
+				Robot.drivetrain.cancelMotion();
+				
+				Pose pose = new Pose(new Point2D(0, 0), 0);
+				
+				MotionPathStraight forwardDrive = new MotionPathStraight(pose, .5,1, 1);
+				
+				Robot.drivetrain.addControllerMotion(forwardDrive);
+				
+				Robot.drivetrain.startMotion();
+			}
+			
+			@Override
 			State run() {
-				// TODO Moves the robot away from the peg
+				if(Robot.drivetrain.isControllerFinished())
+				return END;
+				
 				return super.run();
 			}
 		},
 		END {
 			@Override
+			void init() {
+				super.init();
+			}
+			
+			@Override
 			State run() {
-				// TODO Auto-generated method stub
 				return State.LOOKING;
 			}
 		};
@@ -76,12 +140,6 @@ public class AutoGearPlacement extends Command {
 		State run() {
 			return this;
 		}
-
-	}
-
-	public AutoGearPlacement() {
-		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
 	}
 
 	// Called just before this Command runs the first time
@@ -110,11 +168,6 @@ public class AutoGearPlacement extends Command {
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
 		return false;
-	}
-
-	// Called once after isFinished returns true
-	protected void end() {
-
 	}
 
 	// Called when another command which requires one or more of the same
